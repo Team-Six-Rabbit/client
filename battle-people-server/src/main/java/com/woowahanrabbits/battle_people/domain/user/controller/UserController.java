9da9782bf6142dbf2e9023e9dec5e17fc15d77db
Yangseyeon <seyeon0207@gmail.com>
2024-07-16 13:31:05 +0900

    [feat] complete swagger setting

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/controller/UserController.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.woowahanrabbits.battle_people.domain.user.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+
+    @GetMapping
+    public List<?> getAllUsers() {
+        return null;
+    }
+
 }
