f53471cd5c99b87709174c4bd77893797f9a1da2
5ooyeon <melthetensai@gmail.com>
2024-07-16 14:31:09 +0900

    [feat] add getting battle list

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/battle/service/BattleService.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/battle/service/BattleService.java
@@ -1,5 +1,7 @@
 package com.woowahanrabbits.battle_people.domain.battle.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.woowahanrabbits.battle_people.domain.battle.dto.BattleRegistDto;
@@ -7,5 +9,5 @@ import com.woowahanrabbits.battle_people.domain.battle.dto.BattleRegistDto;
 public interface BattleService {
 	public void registBattle(BattleRegistDto battleRegistDto);
 
-	// List<?> getBattleList(String type, int userId);
+	List<?> getBattleList(String type, long userId);
 }
