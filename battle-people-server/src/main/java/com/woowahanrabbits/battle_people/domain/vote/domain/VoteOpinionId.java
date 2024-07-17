017dc28e4b276c9fe6f030062d4cb1b40be0f2c6
Yangseyeon <seyeon0207@gmail.com>
2024-07-17 15:05:56 +0900

    [Fix] foreign key error fix
    - battle board <-> vote info
    - vote opinion <-> user

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/domain/VoteOpinionId.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/domain/VoteOpinionId.java
@@ -5,5 +5,23 @@ import lombok.Data;
 @Data
 public class VoteOpinionId {
     private int voteOpinionIndex;
-    private Long VoteInfoId;
+    private Long voteInfoId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VoteOpinionId that = (VoteOpinionId) o;
+
+        if (voteOpinionIndex != that.voteOpinionIndex) return false;
+        return voteInfoId.equals(that.voteInfoId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = voteOpinionIndex;
+        result = 31 * result + voteInfoId.hashCode();
+        return result;
+    }
 }
