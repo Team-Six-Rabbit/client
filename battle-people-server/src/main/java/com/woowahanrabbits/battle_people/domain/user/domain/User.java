162b99af5d5b7829df4c338dbe2a8219ed7f6900
5ooyeon <melthetensai@gmail.com>
2024-07-17 14:44:19 +0900

    [Merge] Merge User, BattleBoard

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/domain/User.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/domain/User.java
@@ -2,14 +2,8 @@ package com.woowahanrabbits.battle_people.domain.user.domain;
 
 import java.time.LocalDate;
 
-<<<<<<< HEAD
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-=======
+
 import jakarta.persistence.*;
->>>>>>> 241f410ded449e887607455827aaacbeb8710976
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,20 +12,7 @@ import lombok.Setter;
 @Setter
 public class User {
 
-<<<<<<< HEAD
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
 
-	private String email;
-	private String password;
-	private String nickname;
-	private String img_url;
-	private int rating;
-	private String access_token;
-	private LocalDate penalty_start_date;
-	private LocalDate penalty_end_date;
-=======
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -53,5 +34,5 @@ public class User {
     private String access_token;
     private LocalDate penalty_start_date;
     private LocalDate penalty_end_date;
->>>>>>> 241f410ded449e887607455827aaacbeb8710976
+
 }
