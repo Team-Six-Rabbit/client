3d399dc3a5fadf7a21d5924051a10ab7787302c0
yunjeong <dbswjd1413@naver.com>
2024-07-25 09:52:42 +0900

    Design: chatBox의 높이 수정 - tailwind.config.js에 height 설정 추가 - chatBox 높이를 %로 flexible하게 수정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,15 +4,28 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				orange: '#F66C23',
-				blue: '#0B68EC',
-				yellow: '#FBCA27',
-				olive: '#B4CC38',
+				orange: "#F66C23",
+				blue: "#0B68EC",
+				yellow: "#FBCA27",
+				olive: "#B4CC38",
 			},
 			scale: {
-        '120': '1.2',
-      },
+				"120": "1.2",
+			},
+			height: {
+				"116": "28rem", // ?px
+				"128": "32rem", // 512px
+				"144": "36rem", // 576px
+				"160": "40rem", // 640px
+				"192": "48rem", // 768px
+				"224": "56rem", // 896px
+				"8/9": "88.888889%",
+				"15/16": "93.75%",
+			},
+			clipPath: {
+				trapezoid: "polygon(25% 0%, 75% 0%, 100% 100%, 0% 100%)",
+			},
 		},
 	},
-	plugins: [],
+	plugins: [require("tailwind-scrollbar-hide")],
 };
