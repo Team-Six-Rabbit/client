5246c8b1b149c0265a0e2afab508644f579e8c55
gyeongmann <hkc8857@naver.com>
2024-08-02 17:47:16 +0900

    Feat: 로그인 페이지 오류 메시지 생성
    - 미 입력 시 입력하도록 메시지 생성
    - 로그인 실패 새 버튼 위에 메시지 생성

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,15 +3,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthInput, AuthSubmitBtn } from "@/components/auth/AuthFormComponent";
 import GoogleLoginButton from "@/components/auth/googleLoginBtn";
 import { LoginRequest } from "@/types/api";
-import { authService } from "@/services/userAuthService";
+import { login } from "@/services/userAuthService";
+import { createLiveStateBorder } from "@/utils/textBorder"; // textBorder import
 
 interface CustomCSSProperties extends React.CSSProperties {
 	textShadow?: string;
 }
 
 const headingTagStyles: CustomCSSProperties = {
-	textShadow:
-		"-4px -4px 0 black, 4px -4px 0 black, -4px 4px 0 black, 4px 4px 0 black",
+	...createLiveStateBorder("black", 4),
 };
 
 function LoginPage() {
@@ -21,20 +21,57 @@ function LoginPage() {
 		password: "",
 	});
 
+	const [errors, setErrors] = useState<{
+		email?: string;
+		password?: string;
+		general?: string;
+	}>({}); // errors 상태 추가
+	const [doShake, setDoShake] = useState<boolean>(false);
+
 	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = event.target;
 		setFormValues({
 			...formValues,
 			[name]: value,
 		});
+		setErrors((prevErrors) => ({ ...prevErrors, [name]: "", general: "" })); // 입력 변경 시 에러 초기화
 	};
 
 	const doLogin = async () => {
+		const newErrors: { email?: string; password?: string; general?: string } =
+			{};
+
+		if (!formValues.email) {
+			newErrors.email = "이메일을 입력해주세요."; // 이메일 입력 요구 메시지
+		}
+		if (!formValues.password) {
+			newErrors.password = "비밀번호를 입력해주세요."; // 비밀번호 입력 요구 메시지
+		}
+
+		if (Object.keys(newErrors).length > 0) {
+			setErrors(newErrors);
+			setDoShake(true);
+			setTimeout(() => {
+				setDoShake(false);
+			}, 500);
+			return;
+		}
+
 		try {
-			await authService.login(formValues);
+			await login(formValues);
 			navigator("/");
 		} catch (err) {
-			console.error("로그인 실패"); // TODO: 로그인 실패 시 처리
+			// console.error("로그인 실패");
+			// // TODO: 로그인 실패 시 처리
+			// setDoShake(true);
+			// setTimeout(() => {
+			// 	setDoShake(false);
+			// }, 500);
+			setErrors({ general: "로그인에 실패했습니다." }); // 로그인 실패 메시지
+			setDoShake(true);
+			setTimeout(() => {
+				setDoShake(false);
+			}, 500);
 		}
 	};
 
@@ -47,23 +84,48 @@ function LoginPage() {
 				>
 					로그인
 				</h1>
-				<AuthInput
-					label="이메일"
-					type="email"
-					name="email"
-					value={formValues.email}
-					onChange={handleInputChange}
-					placeholder="이메일"
-				/>
-				<AuthInput
-					label="비밀번호"
-					type="password"
-					name="password"
-					value={formValues.password}
-					onChange={handleInputChange}
-					placeholder="비밀번호"
-				/>
-				<AuthSubmitBtn text="로그인" onClick={doLogin} />
+				<div className="mb-4 relative">
+					<AuthInput
+						label="이메일"
+						type="email"
+						name="email"
+						value={formValues.email}
+						onChange={handleInputChange}
+						placeholder="이메일"
+					/>
+					{errors.email && (
+						<div className="text-red-500 text-xs absolute right-0 top-0 mt-1 mr-2">
+							{errors.email} {/* 이메일 에러 메시지 */}
+						</div>
+					)}
+				</div>
+				<div className="mb-4 relative">
+					<AuthInput
+						label="비밀번호"
+						type="password"
+						name="password"
+						value={formValues.password}
+						onChange={handleInputChange}
+						placeholder="비밀번호"
+					/>
+					{errors.password && (
+						<div className="text-red-500 text-xs absolute right-0 top-0 mt-1 mr-2">
+							{errors.password} {/* 비밀번호 에러 메시지 */}
+						</div>
+					)}
+				</div>
+				<div id="submit-btn" className="relative">
+					<AuthSubmitBtn
+						text="로그인"
+						onClick={doLogin}
+						className={doShake ? "shake" : ""}
+					/>
+					{errors.general && (
+						<div className="text-red-500 text-xs absolute right-0 -top-1 mt-1 mr-2">
+							{errors.general} {/* 일반 에러 메시지 */}
+						</div>
+					)}
+				</div>
 				<GoogleLoginButton />
 				<div className="flex justify-between mt-4">
 					<Link
