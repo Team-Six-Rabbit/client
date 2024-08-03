1c791f21a245cf5ec7881c80854b3e64f48ff863
5ooyeon <melthetensai@gmail.com>
2024-08-03 15:46:41 +0900

    Refactor: merge two same methods in VoteInfoRepository
    - VoteInfoRepository 내 find~ 과 findAll을 하나로 합쳤습니다.

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/infrastructure/VoteInfoRepository.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/infrastructure/VoteInfoRepository.java
@@ -3,22 +3,12 @@ package com.woowahanrabbits.battle_people.domain.vote.infrastructure;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.woowahanrabbits.battle_people.domain.vote.domain.VoteInfo;
-import com.woowahanrabbits.battle_people.domain.vote.dto.GetVoteInfoWithUserCountDto;
 
 @Repository
 public interface VoteInfoRepository extends JpaRepository<VoteInfo, Long> {
-
-	@Procedure(procedureName = "GetVoteInfoWithUserCount")
-	Page<GetVoteInfoWithUserCountDto> getVoteInfoWithUserCount(@Param("userId") Long userId, Pageable pageable);
-
-	Page<VoteInfo> findByCurrentStateAndCategory(int status, Integer category, Pageable pageable);
-
-	Page<VoteInfo> findByCurrentState(int status, Pageable pageable);
 
 	Page<VoteInfo> findAllByCurrentState(int currentState, Pageable pageable);
 
