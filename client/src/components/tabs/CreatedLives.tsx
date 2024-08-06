328f7c7cc33e27ed89d8d827c076bd8258a23424
gyeongmann <hkc8857@naver.com>
2024-08-06 11:08:15 +0900

    Fix: 빌드 실패 오류 해결
    - 원형 그래프 문제 해결
    	declarations.d.ts 파일 생성 및 코드 추가
    	tsconfig.json 수정

--- client/src/components/tabs/CreatedLives.tsx
+++ client/src/components/tabs/CreatedLives.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function CreatedLives() {
 	return (
 		<div className="created-lives">
--- client/src/components/tabs/WinRate.tsx
+++ client/src/components/tabs/WinRate.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { CircularProgressBar } from "@tomickigrzegorz/react-circular-progress-bar";
 
 function WinRate() {
