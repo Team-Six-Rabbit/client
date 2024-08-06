e8e06bd79795a2efe4dbd04ea6a24eaa6be16b21
gyeongmann <hkc8857@naver.com>
2024-08-06 14:28:51 +0900

    Chore: 불필요한 sysout 삭제

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/controller/AuthController.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/controller/AuthController.java
@@ -34,7 +34,6 @@ public class AuthController {
 
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
-		System.out.println("do login");
 		User user = userService.login(loginRequest);
 
 		long userId = user.getId();
