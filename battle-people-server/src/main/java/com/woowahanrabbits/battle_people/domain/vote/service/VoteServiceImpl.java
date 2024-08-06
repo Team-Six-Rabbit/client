cd2ace5e93f7f038abec3456d04c8f7962e19106
5ooyeon <melthetensai@gmail.com>
2024-08-06 17:36:25 +0900

    Feat: 배틀 및 밸런스 게임 종료 관련 로직 추
    - 스케쥴러를 사용하여 종료시간 및 모집시간 기준으로 상태 변경 및 투표
      종료 추가

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/service/VoteServiceImpl.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/vote/service/VoteServiceImpl.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woowahanrabbits.battle_people.domain.battle.infrastructure.BattleBoardRepository;
@@ -12,12 +18,18 @@ import com.woowahanrabbits.battle_people.domain.live.dto.RedisTopicDto;
 import com.woowahanrabbits.battle_people.domain.user.domain.User;
 import com.woowahanrabbits.battle_people.domain.user.infrastructure.UserRepository;
 import com.woowahanrabbits.battle_people.domain.vote.domain.UserVoteOpinion;
+import com.woowahanrabbits.battle_people.config.AppProperties;
+import com.woowahanrabbits.battle_people.domain.battle.domain.BattleBoard;
+import com.woowahanrabbits.battle_people.domain.battle.infrastructure.BattleApplyUserRepository;
+import com.woowahanrabbits.battle_people.domain.battle.infrastructure.BattleRepository;
+import com.woowahanrabbits.battle_people.domain.battle.service.BattleService;
 import com.woowahanrabbits.battle_people.domain.vote.domain.VoteInfo;
 import com.woowahanrabbits.battle_people.domain.vote.domain.VoteOpinion;
 import com.woowahanrabbits.battle_people.domain.vote.dto.CurrentVoteResponseDto;
 import com.woowahanrabbits.battle_people.domain.vote.dto.UserWinHistory;
 import com.woowahanrabbits.battle_people.domain.vote.dto.VoteOpinionDtoWithVoteCount;
 import com.woowahanrabbits.battle_people.domain.vote.dto.VoteRequest;
+import com.woowahanrabbits.battle_people.domain.vote.infrastructure.UserVoteOpinionRepository;
 import com.woowahanrabbits.battle_people.domain.vote.infrastructure.UserVoteOpinionRepository;
 import com.woowahanrabbits.battle_people.domain.vote.infrastructure.VoteInfoRepository;
 import com.woowahanrabbits.battle_people.domain.vote.infrastructure.VoteOpinionRepository;
@@ -35,6 +47,12 @@ public class VoteServiceImpl implements VoteService {
 	private final UserRepository userRepository;
 	private final RedisTemplate<String, String> redisTemplate;
 	private final ObjectMapper objectMapper;
+	private final UserVoteOpinionRepository userVoteOpinionRepository;
+
+	private final BattleRepository battleRepository;
+	private final BattleApplyUserRepository battleApplyUserRepository;
+	private final BattleService battleService;
+	private final AppProperties appProperties;
 
 	@Override
 	public void addVoteInfo(VoteInfo voteInfo) {
@@ -153,6 +171,60 @@ public class VoteServiceImpl implements VoteService {
 			100 - (100 * voteCountOpt1 / totalCount)));
 
 		return new CurrentVoteResponseDto(voteCountOpt1 + voteCountOpt2, opinions);
+	}
+
+	@Scheduled(cron = "0 * * * * *")
+	@Transactional
+	@Override
+	public void updateCurrentState() {
+		Date now = new Date();
+		System.out.println(now.toString());
+
+		//밸런스게임 종료 후 결과 도출 로직
+		List<VoteInfo> balanceGame = voteInfoRepository.findByEndDateBeforeAndCurrentState(now, 6);
+		for (VoteInfo voteInfo : balanceGame) {
+			voteInfo.setCurrentState(7);
+
+			//vote Opinion 의 final count 계산 로직
+			List<VoteOpinion> opinions = voteOpinionRepository.findByVoteInfoId(voteInfo.getId());
+			for (int idx = 0; idx < 2; idx++) {
+				int finalCnt = userVoteOpinionRepository.findByVoteInfoIdAndVoteInfoIndex(voteInfo.getId(), idx).size();
+				opinions.get(idx).setFinalCount(finalCnt);
+				voteOpinionRepository.save(opinions.get(idx));
+			}
+
+			voteInfoRepository.save(voteInfo);
+		}
+
+		//라이브 처리 로직
+		Calendar liveStartCalendar = Calendar.getInstance();
+		liveStartCalendar.setTime(now);
+		liveStartCalendar.add(Calendar.MINUTE, 21);
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(now);
+		calendar.add(Calendar.DATE, 3);
+
+		List<VoteInfo> battle = voteInfoRepository.findByStartDateBeforeAndCurrentState(liveStartCalendar.getTime(), 2);
+		for (VoteInfo voteInfo : battle) {
+			//참여신청한 수 계산
+			BattleBoard battleBoard = battleRepository.findByVoteInfoId(voteInfo.getId());
+			int currentPeopleCount = battleApplyUserRepository.countByBattleBoardId(battleBoard.getId());
+
+			if (currentPeopleCount >= appProperties.getMinPeopleCount()) {
+				//썸네일 출력
+				battleService.createThumbnail(battleBoard.getId());
+				voteInfo.setCurrentState(3);
+				voteInfoRepository.save(voteInfo);
+			} else if (currentPeopleCount < appProperties.getMinPeopleCount()) {
+				//밸런스 게임으로 이동
+				voteInfo.setStartDate(now);
+				voteInfo.setEndDate(calendar.getTime());
+				voteInfo.setCurrentState(6);
+				voteInfoRepository.save(voteInfo);
+			}
+
+		}
+
 	}
 
 }
