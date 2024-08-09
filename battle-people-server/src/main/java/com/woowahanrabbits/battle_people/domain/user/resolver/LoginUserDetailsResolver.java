d7c5b94a0dbff78d97bbc3b09d4c0c7437e6d65f
gyeongmann <hkc8857@naver.com>
2024-08-10 01:12:08 +0900

    Fix: ArgumentResolver 타입 검증 로직 추가
    - domain/user/resolver/LoginUserDetailsResolver
    	securityService.getAuthentication()의 반환 타입이 User이므로
    	User.class가 아닌 경우 log 출력 및 null 반환
    - Join 이후 response 값 수정
    	User -> BasicUserDto

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/resolver/LoginUserDetailsResolver.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/resolver/LoginUserDetailsResolver.java
@@ -7,12 +7,15 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import com.woowahanrabbits.battle_people.domain.user.domain.User;
 import com.woowahanrabbits.battle_people.domain.user.service.SecurityService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class LoginUserDetailsResolver implements HandlerMethodArgumentResolver {
 
 	private final SecurityService securityService;
@@ -27,6 +30,18 @@ public class LoginUserDetailsResolver implements HandlerMethodArgumentResolver {
 		ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest,
 		WebDataBinderFactory binderFactory) {
+
+		Class<?> expectedType = User.class;
+
+		log.debug("Resolving argument for parameter: {} with expected type: {}", parameter.getParameterName(),
+			expectedType.getName());
+
+		if (!expectedType.isAssignableFrom(parameter.getParameterType())) {
+			log.warn("Parameter type mismatch. Expected: {}, Actual: {}", expectedType.getName(),
+				parameter.getParameterType().getName());
+			return null; // 타입이 맞지 않으면 null 반환
+		}
+
 		return securityService.getAuthentication();
 	}
 }
