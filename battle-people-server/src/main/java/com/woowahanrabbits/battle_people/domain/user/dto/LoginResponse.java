b77234de30e063cc5c554c33943d748c5b839f4b
gyeongmann <hkc8857@naver.com>
2024-08-09 14:31:04 +0900

    Fix: LoginResponse 수정
    - 유저 고유 아이디, access 만료 시간 추가

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/dto/LoginResponse.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/domain/user/dto/LoginResponse.java
@@ -1,5 +1,9 @@
 package com.woowahanrabbits.battle_people.domain.user.dto;
 
+import java.util.Date;
+
+import com.woowahanrabbits.battle_people.domain.user.domain.User;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponse {
+	private Long id;
 	private String email;
 	private String nickname;
-	private int rating;
 	private String imgUrl;
+	private Integer rating;
+	private Date accessExpiration;
+
+	public LoginResponse(User user, Date accessExpiration) {
+		this.id = user.getId();
+		this.email = user.getEmail();
+		this.nickname = user.getNickname();
+		this.imgUrl = user.getImgUrl();
+		this.rating = user.getRating();
+		this.accessExpiration = accessExpiration;
+	}
 }
