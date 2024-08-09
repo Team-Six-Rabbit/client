68504d6372e30c087ed660e357130b360657474d
윤성하 <dbstjdgk1541@gmail.com>
2024-08-09 14:40:30 +0900

    Chore: 안쓰는 서비스 함수 제거

--- client/src/services/openviduService.ts
+++ client/src/services/openviduService.ts
@@ -1,20 +1,6 @@
 import axiosInstance from "@/services/axiosInstance";
 import { ApiResponse } from "@/types/api";
 
-export const createSession = async (
-	battleId: string,
-): Promise<ApiResponse<string>> => {
-	try {
-		const response = await axiosInstance.post<ApiResponse<string>>(
-			`/openvidu/create-session?battleId=${battleId}`,
-		);
-		return response.data; // roomId를 string으로 받음
-	} catch (error) {
-		console.error("Failed to create session:", error);
-		throw error;
-	}
-};
-
 export const getTokenAndIndex = async (
 	battleId: string,
 ): Promise<ApiResponse<{ token: string; index: number }>> => {
