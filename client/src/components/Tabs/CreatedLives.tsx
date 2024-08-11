37d7fa3533ecd5df052d2f46c7bffdbdcfb9b5c9
gyeongmann <hkc8857@naver.com>
2024-08-11 18:20:23 +0900

    Fix: 개최한 라이브 날짜 항목 수정

--- client/src/components/Tabs/CreatedLives.tsx
+++ client/src/components/Tabs/CreatedLives.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import "@/assets/styles/scrollbar.css";
 import { authService } from "@/services/userAuthService";
 import { convertToTimeZone } from "@/utils/dateUtils";
-import formatTime from "@/utils/formatTime";
 
 const CreatedLivesContainer = styled.div`
 	width: 100%;
@@ -41,6 +40,18 @@ const StatusCircle = styled.span<{ statusColor: string }>`
 	background-color: ${({ statusColor }) => statusColor};
 `;
 
+const TitleContainer = styled.div`
+	flex-grow: 1;
+	text-align: left;
+`;
+
+const DateStatusContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	gap: 10px; /* 날짜와 상태 아이콘 사이의 간격 */
+`;
+
 interface CreatedLive {
 	id: string;
 	title: string;
@@ -64,17 +75,12 @@ function CreatedLives() {
 						"Asia/Seoul",
 					);
 
-					// 시간 부분을 초로 변환한 후 포맷팅
-					const dateTimeParts = formattedDate.split(" ");
-					const timeParts = dateTimeParts[1].split(":");
-					const totalSeconds =
-						parseInt(timeParts[0], 10) * 3600 + parseInt(timeParts[1], 10) * 60;
-					const formattedTime = formatTime(totalSeconds);
+					const dateOnly = formattedDate.split(" ")[0]; // 'YYYY-MM-DD'
 
 					return {
 						id: String(index + 1),
 						title: live.title,
-						date: `${dateTimeParts[0]} ${formattedTime}`, // 'YYYY-MM-DD HH:MM'
+						date: `${dateOnly}`, // 'YYYY-MM-DD HH:MM'
 						statusColor: live.isWin ? "#BDE3FF" : "#FFC7C2",
 					};
 				});
@@ -102,9 +108,11 @@ function CreatedLives() {
 			<CreatedLivesList>
 				{lives.map((live) => (
 					<CreatedLivesListItem key={live.id}>
-						<span>{live.title}</span>
-						<Date>{live.date}</Date>
-						<StatusCircle statusColor={live.statusColor} />
+						<TitleContainer>{live.title}</TitleContainer>
+						<DateStatusContainer>
+							<Date>{live.date}</Date>
+							<StatusCircle statusColor={live.statusColor} />
+						</DateStatusContainer>
 					</CreatedLivesListItem>
 				))}
 			</CreatedLivesList>
