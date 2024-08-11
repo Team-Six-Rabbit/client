35a645e96bc6ab3ac361eec0b6857d62b868d292
MustSave <dbstjdgk1541@gmail.com>
2024-08-11 11:56:25 +0900

    Feat: 유저 정보 만료 처리

--- client/src/stores/userAuthStore.ts
+++ client/src/stores/userAuthStore.ts
@@ -20,15 +20,9 @@ export const useAuthStore = create<AuthState & AuthAction>()(
 		(set) => ({
 			isLogin: false,
 			user: null,
-			login: (
-				user: DetailUserInfo,
-				expireAt: Date = new Date(Date.now() + 3600000),
-			) => {
+			login: (user: DetailUserInfo) => {
 				set({ isLogin: true, user });
-				sessionStorage.setItem(
-					"user",
-					JSON.stringify({ user, expireAt: expireAt.toISOString() }),
-				);
+				sessionStorage.setItem("user", JSON.stringify({ user }));
 			},
 			logout: () => {
 				set({ isLogin: false, user: null });
@@ -46,8 +40,8 @@ export const useAuthStore = create<AuthState & AuthAction>()(
 // 페이지가 새로고침될 때 유저 정보를 세션 스토리지에서 가져와서 상태를 갱신
 const storedUser = sessionStorage.getItem("user");
 if (storedUser) {
-	const { user, expireAt } = JSON.parse(storedUser);
-	if (new Date() < new Date(expireAt)) {
+	const { user } = JSON.parse(storedUser);
+	if (new Date() < new Date(user.accessExpiration)) {
 		useAuthStore.getState().login(user);
 	} else {
 		sessionStorage.removeItem("user");
