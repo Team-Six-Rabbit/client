88d1762a41535cd8aa24e1c27b5f9ad266cd8372
gyeongmann <hkc8857@naver.com>
2024-08-11 14:37:07 +0900

    Fix: WebMvcConfig 수정

--- battle-people-server/src/main/java/com/woowahanrabbits/battle_people/config/WebMvcConfig.java
+++ battle-people-server/src/main/java/com/woowahanrabbits/battle_people/config/WebMvcConfig.java
@@ -30,9 +30,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/swagger-ui.html**")
 			.addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**")
-			.addResourceLocations("classpath:/META-INF/resources/webjars/");
-		registry.addResourceHandler("/user/images/**")
-			.addResourceLocations("file:" + uploadDir + "/");
 	}
 }
